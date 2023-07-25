b24e11417b8aba4cd275233e322ace05338a809f
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2023-07-25 15:33:20 +0200

    another commit

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
+from contacts import views
 
 urlpatterns = [
-
+    path('contacts/', views.all_Contacts())
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Contact
+from .serilalizers import ContactSerializer
+
 
 # Create your views here.
+
+def all_Contacts(request):
+    contacts = Contact.objects.all()
+    serializer = ContactSerializer(contacts, many=True)
+    return JsonResponse(serializer.data)
